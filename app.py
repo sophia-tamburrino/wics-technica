b0cc3c4ab233e77f1f3287254740e6c83acee7ba
Sophia Tamburrino <sophia.tamburrino>
2025-11-15 20:41:50 -0500

    quiz working

--- app.py
+++ app.py
@@ -51,10 +51,70 @@ def flashcards(path):
     print("Final:", df)
     return df
 
-def quiz():
-    
+def quiz(original_df):
 
+    df = pd.DataFrame(columns=["Option 1", "Option 2", "Option 3", "Option 4", "Answer Key"])
 
+    prompt = f"""
+        You are an AI assistant meant to generate a quiz from a user's imported flashcards. Here are the flashcards:
 
+        {original_df}
+        
+        Your task:
+        
+        Generate a 5-question multiple-choice quiz in this format- 
+        **Question 1**
+        "OPTION 1:" <Possible answer>
+        "OPTION 2:" <Possible answer>
+        "OPTION 3:" <Possible answer>
+        "OPTION 4:" <Possible answer>
+        "CORRECT ANSWER:" <Option number with the correct answer>
 
-flashcards('os-notes.txt') #add path, the user-provided CSV
+        Repeat for 5 questions to help this user study. Do not disobey the format and do not add any additional text other than the quiz.
+
+    """
+
+    try:
+        # generate response from llama
+        model_response = ollama.generate(model='llama3.2:3b', prompt=prompt)
+        raw_response = model_response.get('response', '').strip()
+        print(raw_response)
+        #its annoying so we need to use regex.
+        
+        # Each 
+        pattern1 = r'OPTION 1:\s*(.*)'
+        option1 =  re.findall(pattern1, raw_response)
+        print(option1)
+
+        pattern2 = r'OPTION 2:\s*(.*)'
+        option2 =  re.findall(pattern2, raw_response)
+        print(option2)
+
+        pattern3 = r'OPTION 3:\s*(.*)'
+        option3 =  re.findall(pattern3, raw_response)
+        print(option3)
+
+        pattern4 = r'OPTION 4:\s*(.*)'
+        option4 =  re.findall(pattern4, raw_response)
+        print(option4)
+       
+        answers = r'CORRECT ANSWER:\s*(.*)'
+        answerkey =  re.findall(answers, raw_response)
+        print(answerkey)
+
+        df = pd.DataFrame({
+            'Option 1' : option1,
+            'Option 2' : option2,
+            'Option 3' : option3,
+            'Option 4' : option4,
+            'Answer Key' : answerkey,
+        })
+
+    except Exception as e:
+        print("ERROR", e)
+
+    print("Final:", df)
+    return df
+
+df1 = flashcards('os-notes.txt')
+df2 = quiz(df1)
